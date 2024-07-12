892983f295ee3f4cd211151efe7d3194575f1b3d
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-07-12 17:55:53 +0530

    little changes

--- src/Pages/HomeLayout/HomeLayout.component.jsx
+++ src/Pages/HomeLayout/HomeLayout.component.jsx
@@ -37,7 +37,7 @@ const HomeLayout = () => {
     
     useEffect(() => {
         if (error) {
-            navigate("/signin");
+            navigate("/login");
         }
     }, [error, navigate]);
 
